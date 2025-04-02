03ea5972dbf4b8780f53b916d2b21bc2c4d0abf8
matiyoskidane <matiyoskidane24@gmail.com>
2025-04-02 05:16:00 -0400

    add width and height to the weather icon,humidity and wind images,and make them rounded

--- weather-web/src/componentss/WeatherInfo.jsx
+++ weather-web/src/componentss/WeatherInfo.jsx
@@ -5,19 +5,19 @@ function WeatherInfo({ weather }) {
   
     return (
       <div>
-        <img className="mx-auto w-24 h-24"
+        <img className="mx-auto w-40 h-40"
           src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
            alt={weather.weather[0].description}/>
         <h1 className="text-2xl font-bold">{weather.name}</h1>
         <h2 className="text-xl">{Math.round(weather.main.temp)}°C</h2>
         <div className="flex justify-between mt-4">
           <div>
-          <img src={humidityIcon} alt="Humidity" className="w-8 h-8 mx-auto" />
+          <img src={humidityIcon} alt="Humidity" className="rounded-lg w-24 h-24 mx-auto" />
             <p className="font-bold">{weather.main.humidity}%</p>
             <p className="font-bold">Humidity</p>
           </div>
           <div>
-          <img src={windIcon} alt="Wind Speed" className="w-10 h-10 mx-auto" />
+          <img src={windIcon} alt="Wind Speed" className="rounded-full w-24 h-24 mx-auto" />
             <p className="font-bold">{weather.wind.speed} km/h</p>
             <p className="font-bold">Wind</p>
           </div>
